646388444ea3dcb0e6338578ca4cb390fd615f3b
ksh <kangshqwert@naver.com>
2023-03-21 16:44:42 +0900

    [기능추가]: 영화 디테일 페이지 폼 작성, 데이터 받아오기

---
+++ src/features/movie/useMovieDetail.ts
@@ -0,0 +1,14 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
+
+import { detailApi } from '../../apis/movieAPI';
+import { MovieDetail } from '../../types';
+
+export default function useMovieDetail(query: string) {
+  const queryFn = () => detailApi(query);
+
+  return useQuery<AxiosResponse<MovieDetail>, AxiosError>(
+    ['detailApi', query],
+    queryFn,
+  );
+}
